5a93f95e14ea97c0a34cbae5a1ceb803cb8f30d8
papistan <jpapisan@gmail.com>
2018-08-26 11:17:58 -0700

    Adds additional validations for register and login

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,8 @@ const passport = require('passport');
 
 //Load input validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
+
 // Load User model
 const User = require('../../models/User');
 
@@ -65,6 +67,13 @@ router.post('/register', (req, res) => {
 // @desc    Login User / Returning JWToken
 // @access  Public
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const { email, password } = req.body;
 
   // Find user by email
@@ -72,7 +81,8 @@ router.post('/login', (req, res) => {
     .then(user => {
       // check for user
       if (!user) {
-        return res.status(404).json({ email: 'User not found' });
+        errors.email = 'User not found';
+        return res.status(404).json(errors);
       }
 
       // Check password
@@ -93,7 +103,8 @@ router.post('/login', (req, res) => {
             }
           );
         } else {
-          return res.status(400).json({ password: 'Password incorrect' });
+          errors.password = 'Password incorrect';
+          return res.status(400).json(errors);
         }
       });
     })
